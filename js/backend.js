f126bf9107e3ca54c9ab102d4965ec3f15255bb0
Alsu <aaabdra5@gmail.com>
2018-02-22 16:42:58 +0300

    Избавилась от повторяющегося кода

--- js/backend.js
+++ js/backend.js
@@ -1,8 +1,7 @@
 'use strict';
 
 (function () {
-  window.load = function (onLoad, onError) {
-    var URL = 'https://js.dump.academy/kekstagram/data';
+  function setup(onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -10,7 +9,7 @@
       if (xhr.status === 200) {
         onLoad(xhr.response);
       } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        onError(xhr.response);
       }
     });
     xhr.addEventListener('error', function () {
@@ -22,31 +21,19 @@
 
     xhr.timeout = 10000; // 10s
 
+    return xhr;
+  }
+
+  window.load = function (onLoad, onError) {
+    var URL = 'https://js.dump.academy/kekstagram/data';
+    var xhr = setup(onLoad, onError);
     xhr.open('GET', URL);
     xhr.send();
   };
 
   window.save = function (data, onLoad, onError) {
     var URL = ' https://js.dump.academy/kekstagram';
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000; // 10s
-
+    var xhr = setup(onLoad, onError);
     xhr.open('POST', URL);
     xhr.send(data);
   };
